c22397c50326782e4376b15e39775a43dea4639a
Jinwoo Lee <binaryWooo@gmail.com>
2024-09-30 23:19:09 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/kr/husk/HuskApplication.java
@@ -0,0 +1,13 @@
+package kr.husk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/husk/HuskApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.husk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
