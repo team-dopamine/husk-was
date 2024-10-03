c9e810f6551a4f3d7ab3f6bc31bde7f773c3bd3c
Jsplix <jkjin112@gmail.com>
2024-10-03 23:17:27 +0900

    feat: keychain-entity 설정 및 속성 정의
    Co-authored-by: jinlee1703 <binaryWooo@gmail.com>

--- src/main/java/kr/husk/application/auth/entity/User.java
+++ src/main/java/kr/husk/application/auth/entity/User.java
@@ -4,10 +4,14 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.OneToMany;
 import kr.husk.application.auth.type.OAuthProvider;
+import kr.husk.application.keychain.entity.KeyChain;
 import kr.husk.common.entity.BaseEntity;
 
-@Entity
+import java.util.List;
+
+@Entity(name = "user")
 public class User extends BaseEntity {
     @Column(name = "email", nullable = false, updatable = false, length = 50)
     private String email;
@@ -18,4 +22,7 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "oauth_provider", nullable = false)
     private OAuthProvider oAuthProvider;
+
+    @OneToMany
+    private List<KeyChain> keyChains;
 }
