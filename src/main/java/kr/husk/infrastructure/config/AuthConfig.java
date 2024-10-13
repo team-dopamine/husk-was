5b430998945d5f4750a2a85dae068a28443eac56
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-13 14:32:24 +0900

    feat: 인증 관련 상수 및 prefix 환경변수 분리

--- src/main/java/kr/husk/infrastructure/config/AuthConfig.java
+++ src/main/java/kr/husk/infrastructure/config/AuthConfig.java
@@ -2,11 +2,20 @@ package kr.husk.infrastructure.config;
 
 import kr.husk.domain.repository.AuthCodeRepository;
 import kr.husk.infrastructure.persistence.ConcurrentMapAuthCodeRepository;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AuthCodeConfig {
+@ConfigurationProperties(prefix = "husk.auth")
+@Getter
+@Setter
+public class AuthConfig {
+    private long codeExpiration;
+    private String keyPrefix;
+
     @Bean
     public AuthCodeRepository authCodeRepository() {
         return new ConcurrentMapAuthCodeRepository();
