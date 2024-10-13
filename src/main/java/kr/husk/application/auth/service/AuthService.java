2a1392b2c6bf7782cf17befc2cc29b9f9b23e809
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-13 14:32:53 +0900

    feat: 인증 코드 저장 및 검증 로직 구현

---
+++ src/main/java/kr/husk/application/auth/service/AuthService.java
@@ -0,0 +1,29 @@
+package kr.husk.application.auth.service;
+
+import kr.husk.domain.repository.AuthCodeRepository;
+import kr.husk.infrastructure.config.AuthConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final AuthConfig authConfig;
+    private final AuthCodeRepository authCodeRepository;
+
+    public void sendAuthCode(String key, String code) {
+        // 1. 인증코드 생성
+        authCodeRepository.create(key, code, authConfig.getCodeExpiration());
+
+        // 2. 이메일 전송 (추후 구현)
+    }
+
+    public boolean verifyAuthCode(String email, String code) {
+        String savedCode = authCodeRepository.read(authConfig.getKeyPrefix() + email);
+        if (savedCode != null && savedCode.equals(code)) {
+            authCodeRepository.delete(authConfig.getKeyPrefix() + email);
+            return true;
+        }
+        return false;
+    }
+}
