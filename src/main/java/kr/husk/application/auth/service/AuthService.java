1ebe7fd3a53caf0538e86b4da1d9735f39d61643
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-14 01:12:50 +0900

    feat: 회원 가입을 위한 이메일 인증코드를 포함하는 메일 전송 로직 구현

--- src/main/java/kr/husk/application/auth/service/AuthService.java
+++ src/main/java/kr/husk/application/auth/service/AuthService.java
@@ -3,6 +3,8 @@ package kr.husk.application.auth.service;
 import kr.husk.domain.auth.repository.AuthCodeRepository;
 import kr.husk.infrastructure.config.AuthConfig;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
@@ -11,13 +13,20 @@ import java.util.Random;
 @RequiredArgsConstructor
 public class AuthService {
     private final AuthConfig authConfig;
+    private final JavaMailSender mailSender;
     private final AuthCodeRepository authCodeRepository;
 
-    public void sendAuthCode(String key, String code) {
+    public void sendAuthCode(String to) {
         // 1. 인증코드 생성
-        authCodeRepository.create(key, code, authConfig.getCodeExpiration());
+        String authCode = generateAuthCode();
+        authCodeRepository.create(to, authCode, authConfig.getCodeExpiration());
 
-        // 2. 이메일 전송 (추후 구현)
+        // 2. 이메일 전송
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("[HUSK] 회원가입을 위한 인증코드 발송");
+        message.setText("인증코드: " + authCode);
+        mailSender.send(message);
     }
 
     public boolean verifyAuthCode(String email, String code) {
