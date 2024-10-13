dad29729016984ed3fd85eda35257c8abca7766a
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-14 02:03:54 +0900

    refactor: 이메일 인증코드 전송 메서드 분리 및 예외 처리 로직 추가

--- src/main/java/kr/husk/application/auth/service/AuthService.java
+++ src/main/java/kr/husk/application/auth/service/AuthService.java
@@ -1,34 +1,47 @@
 package kr.husk.application.auth.service;
 
 import kr.husk.domain.auth.repository.AuthCodeRepository;
+import kr.husk.domain.auth.service.UserService;
 import kr.husk.infrastructure.config.AuthConfig;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthService {
     private final AuthConfig authConfig;
     private final JavaMailSender mailSender;
+    private final UserService userService;
     private final AuthCodeRepository authCodeRepository;
 
-    public void sendAuthCode(String to) {
-        // 1. 인증코드 생성
+    @Transactional
+    public String sendAuthCode(String to) {
+        if (userService.isExist(to)) {
+            throw new IllegalArgumentException("이미 가입된 이메일입니다.");
+        }
+
         String authCode = generateAuthCode();
         authCodeRepository.create(to, authCode, authConfig.getCodeExpiration());
 
-        // 2. 이메일 전송
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("[HUSK] 회원가입을 위한 인증코드 발송");
-        message.setText("인증코드: " + authCode);
-        mailSender.send(message);
+        try {
+            sendEmail(to, authCode);
+            log.info("인증 코드가 성공적으로 전송되었습니다. 이메일: {}", to);
+        } catch (Exception e) {
+            log.error("이메일 전송 실패. 이메일: {}", to, e);
+            throw new RuntimeException("이메일 전송에 실패했습니다.");
+        }
+
+        return authCode;
     }
 
+    @Transactional
     public boolean verifyAuthCode(String email, String code) {
         String savedCode = authCodeRepository.read(authConfig.getKeyPrefix() + email);
         if (savedCode != null && savedCode.equals(code)) {
@@ -40,5 +53,13 @@ public class AuthService {
 
     private String generateAuthCode() {
         return String.format("%06d", new Random().nextInt(999999));
+    }
+
+    private void sendEmail(String to, String authCode) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("[HUSK] 회원가입을 위한 인증코드 발송");
+        message.setText("인증코드: " + authCode);
+        mailSender.send(message);
     }
 }
