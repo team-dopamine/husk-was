b5e5e33b7cade89ff8dcad2674dbcade459e604e
Jinwoo Lee <binaryWooo@gmail.com>
2024-11-06 02:27:00 +0900

    feat: 사용자 생성 로직 구현

--- src/main/java/kr/husk/domain/auth/service/UserService.java
+++ src/main/java/kr/husk/domain/auth/service/UserService.java
@@ -1,6 +1,8 @@
 package kr.husk.domain.auth.service;
 
+import kr.husk.domain.auth.entity.User;
 import kr.husk.domain.auth.repository.UserRepository;
+import kr.husk.domain.auth.type.OAuthProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +12,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public User create(String email, String password) {
+        return userRepository.save(User.builder()
+                .email(email)
+                .password(password)
+                .oAuthProvider(OAuthProvider.NONE)
+                .build());
+    }
 
     public boolean isExist(String email) {
         return userRepository.findByEmail(email).isPresent();
