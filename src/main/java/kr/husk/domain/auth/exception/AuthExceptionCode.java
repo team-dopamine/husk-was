99183d1682b91fc25dcd55e64fef8443ae78335d
Jsplix <jkjin112@gmail.com>
2025-01-07 10:04:11 +0900

    feat: oauth 인증 관련 예외코드 추가

--- src/main/java/kr/husk/domain/auth/exception/AuthExceptionCode.java
+++ src/main/java/kr/husk/domain/auth/exception/AuthExceptionCode.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 public enum AuthExceptionCode implements ExceptionCode {
     VERIFICATION_CODE_NOT_MATCH(HttpStatus.BAD_REQUEST, "인증 코드가 일치하지 않습니다."),
     EMAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "인증 메일 발송에 실패했습니다."),
-    PASSWORD_MISMATCHED(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.");
+    PASSWORD_MISMATCHED(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
+    GOOGLE_USERINFO_NOTFOUND(HttpStatus.NOT_FOUND, "Google 사용자 정보 요청에 실패했습니다."),
+    ACCESSTOKEN_REQUEST_FAILED(HttpStatus.UNAUTHORIZED, "Google OAuth Access Token 요청에 실패했습니다.");
 
     HttpStatus httpStatus;
     String cause;
