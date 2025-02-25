b09bc98a81fada7574089a1e6160f9d7dad6dd0b
Jsplix <jkjin112@gmail.com>
2025-02-25 23:30:02 +0900

    feat: api를 위한 controller, service 비즈니스 로직 구현

--- src/main/java/kr/husk/presentation/rest/KeyChainController.java
+++ src/main/java/kr/husk/presentation/rest/KeyChainController.java
@@ -6,6 +6,7 @@ import kr.husk.domain.keychain.service.KeyChainService;
 import kr.husk.presentation.api.KeyChainApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +22,11 @@ public class KeyChainController implements KeyChainApi {
     @PostMapping("")
     public ResponseEntity<?> create(HttpServletRequest request, KeyChainDto.Request dto) {
         return ResponseEntity.ok(keyChainService.create(request, dto));
+    }
+
+    @Override
+    @GetMapping("")
+    public ResponseEntity<?> read(HttpServletRequest request) {
+        return ResponseEntity.ok(keyChainService.read(request));
     }
 }
